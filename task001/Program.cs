84d37dd8bf2c4da8eccf9af20b374afe96f384f1
max232st <max232st@gmail.com>
2022-10-13 00:21:24 +0300

    Семинар 4

---
+++ task001/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine(NumberLenght(number));
+
+// double NumberLenght(int number)
+// {
+//     double len = Math.Floor(Math.Log10(number) + 1);
+//     return len;
+// }
+int NumberLenght(int number)
+{
+    int result = 0;
+    for (int num = 0; number > 0; num++)
+    {
+        number /= 10;
+        result++;
+    }
+    return result;
+}
---
+++ task002/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int NumberProizved(int number)
+{
+   int result = 1;
+   for(int i = 1; i <= number; i++)
+   {
+    result = result*i;
+    Console.WriteLine(result);
+   }
+   return result;
+}
+Console.WriteLine($"результат {NumberProizved(number)}");
